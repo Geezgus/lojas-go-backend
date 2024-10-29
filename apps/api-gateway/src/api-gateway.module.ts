b6de206889508806440adb9de1ee4ae9983d9ee2
Gustavo Duarte <gutorap.gh@gmail.com>
2024-10-29 17:24:35 -0300

    mock connection to users microservice

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ApiGatewayController } from './api-gateway.controller'
 import { ApiGatewayService } from './api-gateway.service'
+import { UsersController } from './mock/users.controller'
 
 @Module({
   imports: [],
-  controllers: [ApiGatewayController],
+  controllers: [ApiGatewayController, UsersController],
   providers: [ApiGatewayService],
 })
 export class ApiGatewayModule {}
