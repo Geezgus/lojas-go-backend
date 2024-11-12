2bf8c29d0c102c732ed9a8eb79d6c07aa20c513d
Gustavo Duarte <gutorap.gh@gmail.com>
2024-11-11 23:54:42 -0300

    create user entity

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,29 +1,30 @@
 import { CreateUserDto, PartialUpdateUserDto, UpdateUserDto } from '@lib/users/users.dto'
+import { User } from '@lib/users'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class UsersService {
-  findAll() {
+  findAll(): Promise<User[]> {
     throw new Error('Method not implemented.')
   }
 
-  findOne(id: string) {
+  findOne(id: string): Promise<User | null> {
     throw new Error('Method not implemented.')
   }
 
-  create(dto: CreateUserDto) {
+  create(dto: CreateUserDto): Promise<User> {
     throw new Error('Method not implemented.')
   }
 
-  update(id: string, dto: UpdateUserDto) {
+  update(id: string, dto: UpdateUserDto): Promise<User | null> {
     throw new Error('Method not implemented.')
   }
 
-  partialUpdate(id: string, dto: PartialUpdateUserDto) {
+  partialUpdate(id: string, dto: PartialUpdateUserDto): Promise<User | null> {
     throw new Error('Method not implemented.')
   }
 
-  delete(id: string) {
+  delete(id: string): Promise<User | null> {
     throw new Error('Method not implemented.')
   }
 }
