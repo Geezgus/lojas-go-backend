93d6c04633cbfbe101313829f5d3e62bb1e55383
Stefane Maria <stefane.maria0901@gmail.com>
2025-06-02 18:43:13 -0300

    fix: update start scripts to reflect new directory structure for users, products, and stores apps

--- dist/apps/products/libs/products/src/products.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductsDto = void 0;
-class ProductsDto {
-}
-exports.ProductsDto = ProductsDto;
-//# sourceMappingURL=products.dto.js.map
