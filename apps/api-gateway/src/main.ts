9e39a61cf60f6ee5137e022e5ccc7155a8c3d5ac
Stefane Maria <stefane.maria0901@gmail.com>
2025-06-02 20:10:05 -0300

    Revert "fix: ensure global.crypto is defined before usage in main.ts"
    This reverts commit 28a9f78c6288d594107a5a2220f9b22b9e77f6d0.

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -2,10 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { ApiGatewayModule } from './api-gateway.module'
 
 async function bootstrap() {
-  if (!global.crypto) {
-    global.crypto = require('crypto')
-  }
-
   const app = await NestFactory.create(ApiGatewayModule)
   app.enableCors()
   await app.listen(process.env.PORT ?? 8080)
