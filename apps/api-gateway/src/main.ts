28a9f78c6288d594107a5a2220f9b22b9e77f6d0
Stefane Maria <stefane.maria0901@gmail.com>
2025-06-02 16:41:13 -0300

    fix: ensure global.crypto is defined before usage in main.ts

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { ApiGatewayModule } from './api-gateway.module'
 
 async function bootstrap() {
+  if (!global.crypto) {
+    global.crypto = require('crypto')
+  }
+
   const app = await NestFactory.create(ApiGatewayModule)
   app.enableCors()
   await app.listen(process.env.PORT ?? 8080)
